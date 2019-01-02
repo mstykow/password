958ae5baaf2a1842cd3462ce47b97a166a356eab
Maxim Stykow <46091730+mstykow@users.noreply.github.com>
2019-01-02 15:22:34 -0800

    Optimized code

--- strongPasswordDetection.py
+++ strongPasswordDetection.py
@@ -3,22 +3,24 @@
 
 import re
 
-check1Regex = re.compile(r'[^\n\s]{8,}')    # checks for 8+ letters
-check2Regex = re.compile(r'.*\d.*')         # checks has at least one digit
-check3Regex = re.compile(r'.*[a-z].*')      # checks has at least one lower case character
-check4Regex = re.compile(r'.*[A-Z].*')      # checks has at least one upper case character
+checkRegex = [
+    re.compile(r'[^\n\s]{8,}'),    # checks for 8+ letters
+    re.compile(r'\d'),             # checks has at least one digit
+    re.compile(r'.*[a-z].*'),      # checks has at least one lower case character
+    re.compile(r'.*[A-Z].*')       # checks has at least one upper case character
+]
 
-passwords = ['ImY0urFavorIte','ImY0urF','imy0urfavorite','IMY0URFAVORITE']
+passwords = ['ImY0urFavorIte','ImYourFavorIte','ImY0urF','imy0urfavorite','IMY0URFAVORITE']
 
 def passwordStrength(pw):
-    mo1 = check1Regex.search(pw)
-    mo2 = check2Regex.search(pw)
-    mo3 = check3Regex.search(pw)
-    mo4 = check4Regex.search(pw)
-    if mo1 == None or mo2 == None or mo3 == None or mo4 == None:
-        return 'weak.'
-    else:
-        return 'strong.'
+    for condition in checkRegex:
+        mo = condition.search(pw)
+        if mo == None:
+            ans = 'weak.'
+            break
+        else:
+            ans = 'strong.'
+    return ans
 
 for i in passwords:
     print('Password ' + i + ' is ' + passwordStrength(i))
